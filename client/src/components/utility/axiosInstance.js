cde1d15fde1504ff7fd81ff904289630f18835fc
Codbeeast <vishwakarmapriyanshu9099@gmail.com>
2025-06-22 11:53:06 +0530

    Update axiosInstance.js

--- client/src/components/utility/axiosInstance.js
+++ client/src/components/utility/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios"; 
-const DB_URL=import.meta.env.VITE_DB_URL
+const DB_URL=import.meta.env.VITE_API_URL || 'http://localhost:5000'
 export const axiosInstance = axios.create({
  
     baseURL : DB_URL,
